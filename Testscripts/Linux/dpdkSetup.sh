122fc8b012d4d622c129f80997be4c918ef02e46
Adrian Vladu <avladu@cloudbasesolutions.com>
2019-03-06 19:30:57 +0200

    dpdk: add option to install dpdk from ppa

--- Testscripts/Linux/dpdkSetup.sh
+++ Testscripts/Linux/dpdkSetup.sh
@@ -93,6 +93,20 @@ function install_dpdk () {
 		check_exit_status "git clone $dpdkSrcLink on ${1}" "exit"
 		cd "$dpdkSrcDir"
 		LogMsg "dpdk source on ${1} $dpdkSrcDir"
+	elif [[ $dpdkSrcLink =~ "ppa:" ]];
+	then
+		if [[ $DISTRO_NAME != "ubuntu" && $DISTRO_NAME != "debian" ]];
+		then
+			echo "PPAs are supported only on Debian based distros."
+			SetTestStateAborted
+			exit 1
+		fi
+		ssh "${1}" "add-apt-repository ${dpdkSrcLink} -y"
+		ssh "${1}" ". ${UTIL_FILE} && update_repos"
+		ssh "${1}" ". ${UTIL_FILE} && install_package dpdk"
+		check_exit_status "Install DPDK from ppa ${dpdkSrcLink} on ${1}" "exit"
+		LogMsg "*********Installed DPDK on ${1}********"
+		return
 	else
 		LogMsg "Provide proper link $dpdkSrcLink"
 	fi
